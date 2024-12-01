737cf4c22c21eaffbad9445328e35e42dec41fe0
Paul Malikov <vokilam@gmail.com>
2024-12-01 22:07:49 +0100

    Add firebase dep

--- Ideas/IdeasApp.swift
+++ Ideas/IdeasApp.swift
@@ -6,10 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct IdeasApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State private var store = IdeasStore.mocked
     
     var body: some Scene {
